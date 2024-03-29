3a7d076ecaae85863c8935c2c93de1dbd2895afc
suxin123 <2418213125@qq.com>
2024-03-29 23:34:02 +0800

    命令行解析

--- src/app/component/TransferCard.tsx
+++ src/app/component/TransferCard.tsx
@@ -4,81 +4,73 @@ import {
   CardBody,
   CardFooter,
   Code,
-  Button,
-} from "@nextui-org/react";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import dottedArrow from "/public/imgs/dottedArrow.png";
+  Button
+} from '@nextui-org/react'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import dottedArrow from '/public/imgs/dottedArrow.png'
 
-export default function TransferCard() {
+export default function TransferCard (props: any) {
+  const { type, relayHash } = props
   // const time = dayjs(CreatedAt).format("MMM DD, YYYY, HH:mm");
-  const time = "Jan 19, 2024, 14:22";
+  const time = 'Jan 19, 2024, 14:22'
   const extraData = {
-    from: "1J93t4tZ76hX9i3Qd7aR4yY3F4iGqG6z8z7gPN8oQe",
-    to: "3A3gYZFopmTK1bSVLwsMQDntWQTZARfNXq",
-    gas: "233",
-    toName: "Test",
-  };
+    from: '1J93t4tZ76hX9i3Qd7aR4yY3F4iGqG6z8z7gPN8oQe',
+    to: '3A3gYZFopmTK1bSVLwsMQDntWQTZARfNXq',
+    gas: '233',
+    toName: 'Test'
+  }
   const formatAddress = (s: string) => {
     if (s) {
-      const head = s.slice(0, 4);
-      const tail = s.slice(s.length - 4, s.length);
-      return head + "..." + tail;
+      const head = s.slice(0, 4)
+      const tail = s.slice(s.length - 4, s.length)
+      return head + '...' + tail
     }
-    return "...";
-  };
+    return '...'
+  }
   return (
     <>
-      <Card className="mx-5 mb-5 min-h-screen rounded-[20px] bg-[#404040]">
-        <CardHeader className="relative">
-          <div className="text-[12px] text-[#c6cad6] pl-3 mt-2">{time}</div>
+      <Card className='mx-5 mb-5 h-fit rounded-[20px] bg-[#404040]'>
+        <CardHeader className='relative'>
+          <div className='text-[18px] text-[#c6cad6] pl-3 mt-2'>{time}</div>
         </CardHeader>
-        <CardBody className="px-3 py-0">
-          <div className="p-5 flex flex-row text-center border-b-1 border-[#405aab]">
-            <div className="basis-1/3">
-              <div className="text-[18px] font-bold mb-3">You</div>
+        <CardBody className='px-3 py-0'>
+          <div className='p-5 flex flex-row text-center border-b-1 border-[#405aab]'>
+            <div className='basis-1/3'>
+              <div className='text-[28px] font-bold mb-3'>You</div>
               <Code>{formatAddress(extraData.from)}</Code>
             </div>
-            <div className="basis-1/3">
-              <div className="text-[14px] text-[#4faaeb]">
-                {extraData.gas} USDT{" "}
+            <div className='basis-1/3'>
+              <div className='text-[18px] text-[#4faaeb]'>
+                {extraData.gas} USDT{' '}
               </div>
-              <div className="text-[18px] m-2 h-[27px]">
-                <Image className="m-auto" src={dottedArrow} alt="dottedArrow" />
+              <div className='text-[28px] m-2 h-[27px]'>
+                <Image className='m-auto' src={dottedArrow} alt='dottedArrow' />
               </div>
-              <div className="text-[12px] text-[#819df5]">Direct Transfer</div>
+              <div className='text-[18px] text-[#819df5]'>Gas</div>
             </div>
-            <div className="basis-1/3">
-              <div className="text-[18px] font-bold mb-3">
-                {extraData.toName}
+            <div className='basis-1/3'>
+              <div className='text-[28px] font-bold mb-3'>
+                {relayHash || 'Not Connected'}
               </div>
               <Code>{formatAddress(extraData.to)}</Code>
             </div>
           </div>
         </CardBody>
-        <CardFooter className="gap-3">
-          <div className="flex w-full">
-            <div className="flex-1 flex-grow flex justify-center">
+        <CardFooter className='gap-3'>
+          <div className='flex w-full'>
+            <div className='flex-1 flex-grow flex justify-center'>
               <Button
-                radius="md"
-                size="lg"
-                className="bg-[#4662b6] text-white shadow-lg w-4/5"
+                radius='md'
+                size='lg'
+                className='bg-[#4662b6] text-white shadow-lg w-4/5'
               >
-                View
-              </Button>
-            </div>
-            <div className="flex-1 flex-grow flex justify-center ">
-              <Button
-                radius="md"
-                size="lg"
-                className="bg-[#33aa5c] text-white shadow-lg w-4/5"
-              >
-                Resend
+                Next
               </Button>
             </div>
           </div>
         </CardFooter>
       </Card>
     </>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,80 @@
-import Navbar from "./component/navbar";
-import CommandLine from "./component/commandLine";
-import TransferCard from "./component/transferCard";
+'use client'
 
-export default function Home() {
+import Navbar from './component/navbar'
+import CommandLine from './component/commandLine'
+import TransferCard from './component/transferCard'
+import { useEffect, useState, useRef } from 'react'
+
+export default function Home () {
+  function parsingCommand (command: string) {
+    let result: {
+      type: string | 'add' | 'remove' | 'pull' | 'push' | null
+      relayHash: string
+    } = {
+      type: null,
+      relayHash: ''
+    }
+    const subStrList = [
+      {
+        substr: 'agigit relay add',
+        getResult: (command: string) => {
+          const list = command.split(' ')
+          return {
+            type: 'add',
+            relayHash: list[list.length - 1]
+          }
+        }
+      },
+      {
+        substr: 'agigit relay remove',
+        getResult: (command: string) => {
+          return {
+            type: 'remove',
+            relayHash: ''
+          }
+        }
+      },
+      {
+        substr: 'agigit pull',
+        getResult: (command: string) => {
+          return {
+            type: 'pull'
+          }
+        }
+      },
+      {
+        substr: 'agigit push',
+        getResult: (command: string) => {
+          return {
+            type: 'push'
+          }
+        }
+      }
+    ]
+    for (let item of subStrList) {
+      if (command.includes(item.substr)) {
+        result = { ...result, ...item.getResult(command) }
+        break
+      } else {
+        console.log('error command!')
+      }
+    }
+    return result
+  }
+  const [command, setCommand] = useState('')
+  const [cardDom, setCardDom] = useState<any>()
+  function getTransferCard (params: any) {
+    return <TransferCard {...params} />
+  }
+  useEffect(() => {
+    setCardDom(getTransferCard(parsingCommand(command)))
+  }, [command])
+
   return (
-    <div className="flex min-h-screen flex-col p-24">
+    <div className='flex min-h-screen flex-col p-24'>
       <Navbar />
-      <CommandLine />
-      <TransferCard />
+      <CommandLine onKeyDown={(command: string) => setCommand(command)} />
+      {cardDom}
     </div>
-  );
+  )
 }
