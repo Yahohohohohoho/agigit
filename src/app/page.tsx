e1d7cc9aa0a2d22b37b90375d852756a66cde63a
XinBaoCode <1392368311@qq.com>
2024-03-31 08:47:58 +0800

    feat: 加入首页动效

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { NextUIProvider } from "@nextui-org/react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AppContext } from "./component/wallet/appContext";
 
 const WalletButtons = dynamic(
@@ -16,6 +16,13 @@ const WalletButtons = dynamic(
 export default function Page() {
   const router = useRouter();
   const [loginState, setLoginState] = useState<boolean>(false);
+  const ref = useRef<any>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.style.width = `${ref.current.scrollWidth + 10}px`;
+    }
+  }, []);
 
   useEffect(() => {
     if (loginState) {
@@ -29,7 +36,12 @@ export default function Page() {
         <div
           className={`round-lg bg-[url("/login-bg.png")] bg-cover bg-center bg-no-repeat h-[100vh] items-center justify-center flex`}
         >
-          <WalletButtons setLoginState={setLoginState} />
+          <div className="flex flex-col items-center">
+            <div className="text-[#fff] text-8xl mb-16 type-writer" ref={ref}>
+              Babel - AgiGit 🤩
+            </div>
+            <WalletButtons setLoginState={setLoginState} />
+          </div>
         </div>
       </AppContext>
     </NextUIProvider>
